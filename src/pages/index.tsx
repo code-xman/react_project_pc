22cc4e4303e760a3a34401aff9cf8b7d9ccb1a9c
xiangjie <xiangjie@yunsom.com>
2022-03-25 11:47:20 +0800

    修改ts类型

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
-import './css/common.less'
+import './css/common.less';
 import './index.less';
-import MyMenu from '@/pages/menu/myMenu'
+import MyMenu from '@/pages/menu/myMenu';
 import cn from 'classnames';
+import { ReactChild, ReactFragment, ReactPortal } from 'react';
 
-export default (props:any) => {
+export default (props: {
+  children:
+    | boolean
+    | ReactChild
+    | ReactFragment
+    | ReactPortal
+    | null
+    | undefined;
+}) => {
   return (
     <div className={cn('ys-layout')}>
       <h1 className={cn('top-title')}>YS Demo</h1>
-      <div className={cn('container')} >
+      <div className={cn('container')}>
         <MyMenu></MyMenu>
         <div className={cn('page-box', 'box-border', 'flex')}>
-          { props.children }
+          {props.children}
         </div>
       </div>
     </div>
   );
-}
+};
--- src/pages/menu/myMenu.tsx
+++ src/pages/menu/myMenu.tsx
@@ -1,39 +1,54 @@
 import { history } from 'umi';
 import { Menu } from 'antd';
-import { menuList } from './menu.js'
+import { menuList } from './menu.js';
 
 const { SubMenu } = Menu;
 
 const MyMenu = () => {
-  const menuClick = (src:string) => {
-    history.push(src)
-  }
-
+  const menuClick = (src: string) => {
+    history.push(src);
+  };
 
   return (
-    <div style={{height: '100%'}}>
-
-      <Menu mode="inline" style={{ width: 256, height: '100%', overflowY: 'auto', overflowX: 'hidden', }} >
-        {
-          menuList.map( (item:{id: string,name: string, children?:any}) => {
+    <div style={{ height: '100%' }}>
+      <Menu
+        mode="inline"
+        style={{
+          width: 256,
+          height: '100%',
+          overflowY: 'auto',
+          overflowX: 'hidden',
+        }}
+      >
+        {menuList.map(
+          (item: {
+            id: string;
+            name: string;
+            children: { id: string; name: string; src: string }[];
+          }) => {
             // console.log('item :>> ', item);
             return (
               <SubMenu key={item.id} title={item.name}>
-                {
-                  item.children && item.children.map((cItem:{id: string,name: string, src:string}) => {
-                    // console.log('cItem :>> ', cItem);
-                    return (
-                      <Menu.Item key={cItem.id} onClick={()=>menuClick(cItem.src)}>{cItem.name}</Menu.Item>
-                    )
-                  })
-                }
+                {item.children &&
+                  item.children.map(
+                    (cItem: { id: string; name: string; src: string }) => {
+                      // console.log('cItem :>> ', cItem);
+                      return (
+                        <Menu.Item
+                          key={cItem.id}
+                          onClick={() => menuClick(cItem.src)}
+                        >
+                          {cItem.name}
+                        </Menu.Item>
+                      );
+                    },
+                  )}
               </SubMenu>
-            )
-          })
-        }
+            );
+          },
+        )}
       </Menu>
-      
     </div>
-  )
-}
-export default MyMenu
+  );
+};
+export default MyMenu;
