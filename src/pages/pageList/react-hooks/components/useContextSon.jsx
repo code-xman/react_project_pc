aad2751ddb7d17ac535f53da73bd019c4a82b574
xiangjie <xiangjie@yunsom.com>
2022-06-01 16:41:36 +0800

    优化common样式，usecontext传方法

--- src/pages/pageList/react-hooks/components/useContextSon.jsx
+++ src/pages/pageList/react-hooks/components/useContextSon.jsx
@@ -1,12 +1,25 @@
 import React, { useContext } from 'react';
+import { Button } from 'antd';
 import { myContext } from '../useContext';
 
 const Son = () => {
-  const theme = useContext(myContext);
+  const contextVal = useContext(myContext);
+  const { themes, setthemes } = contextVal;
+  const onClick = () => {
+    setthemes({
+      foreground: '#FFFFFF',
+      background: '#111111',
+    });
+  };
   return (
-    <div style={{ background: theme.background, color: theme.foreground }}>
-      I am styled by theme context!
-    </div>
+    <>
+      <div style={{ background: themes.background, color: themes.foreground }}>
+        I am styled by theme context!
+      </div>
+      <Button className="mg-5" onClick={onClick}>
+        子组件改变主题
+      </Button>
+    </>
   );
 };
 
--- src/pages/pageList/react-hooks/useContext.jsx
+++ src/pages/pageList/react-hooks/useContext.jsx
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Button } from 'antd';
 import Son from './components/useContextSon';
 
-const themes = {
-  light: {
-    foreground: '#000000',
-    background: '#eeeeee',
-  },
-  dark: {
-    foreground: '#ffffff',
-    background: '#222222',
-  },
+const mythemes = {
+  foreground: '#000000',
+  background: '#eeeeee',
 };
 
+// Context 为上下文；可以将父组件的数据传递给子级，且中间组件可以不用进行传递
 export const myContext = React.createContext({});
 
 const Child = () => {
@@ -23,10 +19,24 @@ const Child = () => {
 };
 
 const UseContextDom = () => {
+  const [themes, setthemes] = useState(mythemes);
+  const contextVal = {
+    themes,
+    setthemes,
+  };
+  const onClick = () => {
+    setthemes(mythemes);
+  };
   return (
-    <myContext.Provider value={themes.light}>
-      <Child></Child>
-    </myContext.Provider>
+    <div className="flex-1">
+      {/* 需要传递的数据写入 <XXXXXX.Provider> 的value属性中，可以传入变量和方法 */}
+      <myContext.Provider value={contextVal}>
+        <Child></Child>
+      </myContext.Provider>
+      <Button className="mg-5" onClick={onClick}>
+        父组件重置主题
+      </Button>
+    </div>
   );
 };
 
