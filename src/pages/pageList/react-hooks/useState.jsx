1cb065fffd7def95ad40724a34c3c72f09573547
xiangjie <xiangjie@tansun.com.cn>
2022-10-31 19:49:39 +0800

    useState更新机制

--- src/pages/pageList/react-hooks/useState.jsx
+++ src/pages/pageList/react-hooks/useState.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import cn from 'classnames';
 import { Button } from 'antd';
 /**
@@ -9,6 +9,29 @@ import { Button } from 'antd';
 const UseStateDome = () => {
   // 初始化可以使用函数，此函数只在初始渲染时被调用
   const [a, setA] = useState(() => 0);
+  const [b, setB] = useState({
+    name: '',
+    age: '',
+  });
+
+  const handleB = () => {
+    setA(() => -1);
+    setB(() => {
+      return {
+        name: 'aaa',
+        age: '1111',
+      };
+    });
+
+    /**
+     * 在Hooks中，React并不知道我们调用了几次useState，
+     * 所以React通过将一个Hook对象挂载在memorizedStated上来保存函数组件的state。
+     * react会在后头一起更新，所以像console这样打印出来的数据还是之前的数据
+     */
+    console.log('a :>> ', a);
+    console.log('b :>> ', b);
+  };
+
   return (
     <div className={cn('flex-1')}>
       <Button style={{ marginRight: '10px' }} onClick={() => setA(0)}>
@@ -32,6 +55,11 @@ const UseStateDome = () => {
       </Button>
 
       <p>{a}</p>
+
+      <Button style={{ marginRight: '10px' }} type="primary" onClick={handleB}>
+        handleB
+      </Button>
+      <p>{b.name ? `${b.name}-${b.age}` : 'b'}</p>
     </div>
   );
 };
